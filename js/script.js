bda5663f4ddea7cb4f3fbb8c76e74ff23043f18c
Sora <brenoolieveira1611@gmail.com>
2025-03-05 22:48:13 -0300

    > Style 1.0

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+    - fetch
+    - async 
+    - await 
+    - try 
+    - .then
+    - catch 
+    - method 
+    - Adicionar parâmetros à URL (query params) 
+    - Lidando com Headers (autenticação, tokens, etc.) 
+
+    My key: X9B4-H4N7-SZ5N-R0UH
+
+*/
+
+console.error("!!! TESTE !!!")
+
+
